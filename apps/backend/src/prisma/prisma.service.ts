ac7a51acbf262f6b216055c626904fea2bdef3fa
Jyotish Developer <user@jyotish.app>
2026-01-30 20:43:15 +0530

    Integrated Razorpay, PayPal and Subscription Plans

--- apps/backend/src/prisma/prisma.service.ts
+++ apps/backend/src/prisma/prisma.service.ts
@@ -1,23 +1,21 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    constructor(private config: ConfigService) {
+    constructor() {
         super({
             datasources: {
                 db: {
-                    url: config.get<string>('DATABASE_URL'),
+                    url: process.env.DATABASE_URL,
                 },
             },
         });
     }
 
     async onModuleInit() {
-        const url = this.config.get('DATABASE_URL');
-        if (!url) {
-            throw new Error('DATABASE_URL is not defined in environment variables');
+        if (!process.env.DATABASE_URL) {
+            console.error('DATABASE_URL is missing!');
         }
         await this.$connect();
     }
